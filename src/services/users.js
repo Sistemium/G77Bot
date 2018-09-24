bacd5dbe86b8070e586663a00e9b99dab4947504
Edgar Jan <krodanix@gmail.com>
2018-09-24 20:48:59 +0300

    users

---
+++ src/services/users.js
@@ -0,0 +1,34 @@
+import { hgetallAsync, hsetAsync, hgetAsync } from 'sistemium-telegram/services/redis';
+import map from 'lodash/map';
+
+const USERS_KEY = 'users';
+
+export async function addUser(org, id, data) {
+
+  return hsetAsync(orgKey(org), id, JSON.stringify({
+    ...data,
+    id,
+  }));
+
+}
+
+export async function findAll(org) {
+
+  const data = await hgetallAsync(orgKey(org));
+
+  return map(data, JSON.parse);
+
+}
+
+export async function find(org, id) {
+
+  const data = await hgetAsync(orgKey(org), id);
+
+  return data ? JSON.parse(data) : null;
+
+}
+
+
+function orgKey(org) {
+  return `${USERS_KEY}_${org}`;
+}
